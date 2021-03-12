92805ad222a352355607dd6c1a802c7f51291cb7
David Tepaße <dtepasse@t-online.de>
2021-03-12 10:52:35 +0100

    test - davidtepasse

--- Rucksack.java
+++ Rucksack.java
@@ -8,16 +8,19 @@
 public class Rucksack
 {
     // instance variables - replace the example below with your own
-    //??
-
+    private List<Gegenstand> liste;
     /**
      * Constructor for objects of class Rucksack
      */
     public Rucksack()
     {
         // initialise instance variables
-        //??
+        liste = new List<Gegenstand>();
     }
 
+    public void gegenstandHinzufuegen(Gegenstand g) {
+        liste.insert(g);
+        liste.toFirst();
+    }
     
 }
