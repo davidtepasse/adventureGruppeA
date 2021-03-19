042065d6902d70852854b85e83ca24a798a55078
MoritzKiffmeier <moritz.@kiffmeier.de>
2021-03-19 09:39:54 +0100

    schmied nimmt geld

--- Schmied.java
+++ Schmied.java
@@ -9,6 +9,7 @@ public class Schmied extends Karacter
 {
     // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
     private Korb lager;
+    private Waehrung geld;
 
     /**
      * Konstruktor für Objekte der Klasse Schmied
@@ -18,10 +19,12 @@ public class Schmied extends Karacter
         // Instanzvariable initialisieren
         super("Hauer",100,1);
         lager = new Korb();
+        geld = new Waehrung();
     }
 
     public void reparieren() {
         Waffe temp = lager.waffeAusgeben();
         temp.setZustand(temp.getZustand()+10);
+        geld.verlust(15);
     }
 }
